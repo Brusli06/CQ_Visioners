5f08a6b45730029bbc50c8530728298faa263919
georgegrigore <georgegrigore010@gmail.com>
2025-12-05 23:58:26 +0200

    Delete dataset.py

--- dataset.py
+++
@@ -1,5 +0,0 @@
-import kagglehub
-#path=kagglehub.dataset_download("omkargurav/face-mask-dataset")
-#path = kagglehub.dataset_download("anku5hk/5-faces-dataset")
-path = kagglehub.dataset_download("freak2209/face-data")
-print("Path to dataset:",path)
